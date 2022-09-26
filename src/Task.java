03075f00b510cc63f4aa163964997b881b180d31
7Soldier <reg.fm4@gmail.com>
2022-09-26 05:10:38 +0300

    homework: src: Complete second task
    Reference: IvanMurzin/ITSchoolSamsung2022@28342d0
    
    Co-authored-by: IvanMurzin <ivanmurzin813@gmail.com>
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- src/Task.java
+++ src/Task.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
         firstTask(); // Start first homework task
+        secondTask(); // Start second homework task
     }
 
     // First homework task
@@ -20,5 +21,29 @@ public class Task {
         int next = ((input + 1) % 2 == 0) ? input + 1 : input + 2;
 
         System.out.println(sum + " " + next);
+    }
+
+    // Second homework task
+    public static void secondTask() {
+        System.out.println("===== Task 2nd =====");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please, enter input info: ");
+        String input = scan.nextLine();
+
+        String[] split_input = input.split(" ");
+        int[] nums = new int[3];
+        for (int counter = 0; counter < 3; counter++) {
+            nums[counter] = Integer.parseInt(split_input[counter]);
+        }
+
+        int rub_price = nums[0] * nums[2];
+        int penny_price = nums[1] * nums[2];
+        while (penny_price >= 100) {
+            rub_price += 1;
+            penny_price -= 100;
+        }
+
+        System.out.println(rub_price + " " + penny_price);
     }
 }
